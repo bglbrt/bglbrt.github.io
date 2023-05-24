6a913145aa1bf764a0e4b0fc04a86d01dd5f69fd
bglbrt <82523827+bglbrt@users.noreply.github.com>
2023-05-24 13:48:14 +0200

    Update app.js

--- particles.js-master/demo/js/app.js
+++ particles.js-master/demo/js/app.js
@@ -64,7 +64,7 @@ particlesJS('particles-js',
       }
     },
     "interactivity": {
-      "detect_on": "canvas",
+      "detect_on": "window",
       "events": {
         "onhover": {
           "enable": true,
